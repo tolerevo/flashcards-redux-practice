8c77f0b6da8f494b67c3408f5e324b60e71ee890
tolerevo <tolerevo@hotmail.com>
2024-03-17 07:53:40 -0600

    mend

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -7,17 +7,23 @@ export const topicsSlice = createSlice({
   },
   reducers: {
     addTopic: (state, action) => {
-      const { id, name, action } = action.payload;
+      const { id, name, icon } = action.payload;
       state.topics[id] = {
         id: id,
         name: name,
         icon,
         quizIds: []
       };
-    },
+    }
+  },
+  extraReducers: {
+    "quizzes/addQuiz": (state, action) => {
+      const { topicId, id } = action.payload;
+      state.topics[topicId].quizIds.push(id);
+    }
   }
 });
 
-export const { addTopic } = topicsSlice.actions;
+export const { addTopic, addQuizIdForTopic } = topicsSlice.actions;
 export const selectTopics = (state) => state.topics.topics;
 export default topicsSlice.reducer;
